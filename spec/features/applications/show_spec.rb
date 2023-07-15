97ac05c3f396081e2194f122cea7ccfee4b3a277
andi lovetto <andilovetto@gmail.com>
2023-07-15 15:10:57 -0600

    test: Adds testing for US 4 for searching pets by name

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "applications show page" do
   let!(:shelter1) { Shelter.create!(name:"Paws without Laws", foster_program: false, city: "Denver", rank: 3) }
   let!(:pet_1) { shelter1.pets.create!(name: "Matt", age: 14, breed: "Cat", adoptable: true) }
   let!(:pet_2) { shelter1.pets.create!(name: "Pog", age: 14, breed: "Dog", adoptable: true) }
+  let!(:pet_3) { shelter1.pets.create!(name: "Anetra", age: 3, breed: "Leopard Gecko", adoptable: true) }
   let!(:pet_app_1) { PetApplication.create!(pet_id: pet_1.id, application_id: app_1.id) } 
   let!(:pet_app_2) { PetApplication.create!(pet_id: pet_2.id, application_id: app_1.id) } 
 
@@ -13,14 +14,25 @@ RSpec.describe "applications show page" do
   end
 
   it "displays application information" do
-    expect(page).to have_content("#{app_1.name}")
+    expect(page).to have_content("Name: #{app_1.name}")
     expect(page).to have_content("466 Birch Road Birmingham, Alabama, 35057")
-    expect(page).to have_content("#{app_1.description}")
-    expect(page).to have_content("#{app_1.status}")
+    expect(page).to have_content("Description: #{app_1.description}")
+    expect(page).to have_content("Status: #{app_1.status}")
     expect(page).to have_link(pet_1.name)
     expect(page).to have_link(pet_2.name)
     click_link pet_1.name
     expect(current_path).to eq("/pets/#{pet_1.id}")
+  end
+
+  it "has a section to add pet a pet to the application" do
+    within(".add_pet_to_application") do
+      expect(page).to have_content("Add a Pet to this Application")
+      expect(page).to have_field(:name)
+      fill_in "Search By Pet's Name", with: pet_3.name
+      click_button "Submit"
+      expect(current_path).to eq("/applications/#{app_1.id}")
+      expect(page).to have_content(pet_3.name)
+    end
   end
 
 
