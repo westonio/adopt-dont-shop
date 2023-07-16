2fc695bad505dc84d91af26b4026d65acc9c41a2
andi lovetto <andilovetto@gmail.com>
2023-07-16 14:07:11 -0600

    test: Adds testing for pending applications class method

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -67,5 +67,21 @@ RSpec.describe Shelter, type: :model do
         expect(@shelter_1.pet_count).to eq(3)
       end
     end
+    
+    describe ".shelters_with_pending_applications" do
+      let!(:app_1) { Application.create!(name: "Bob", street_address: "466 Birch Road", city: "Birmingham", state: "Alabama", zip_code: "35057", description: "description", status: "In Progress" ) }
+      let!(:app_2) { Application.create!(name: "Tina Belcher", street_address: "466 Albion Street", city: "New York", state: "New York", zip_code: "10001", description: "We have lots of fun", status: "Pending" ) }
+      let!(:shelter_1) { Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9) }
+      let!(:shelter_2) { Shelter.create(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5) }
+      let!(:shelter_3) { Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10) }
+      let!(:pet_1) { shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true) }
+      let!(:pet_2) { shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true) }
+      let!(:pet_3) { shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true) }
+      let!(:pet_app_1) { PetApplication.create!(pet_id: pet_1.id, application_id: app_2.id) } 
+
+      it "returns array of shelters with pending apps" do
+        expect(Shelter.pending_applications).to eq([shelter_1])
+      end
+    end
   end
 end
