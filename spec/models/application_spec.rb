4250b450a156da346df8b893a809bb4982022d2d
andi lovetto <andilovetto@gmail.com>
2023-07-16 12:14:27 -0600

    test: Adds instance method testing for full address method in application model

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -4,6 +4,16 @@ RSpec.describe Application, type: :model do
   describe "relationships" do
     it { should have_many :pet_applications }
     it { should have_many(:pets).through(:pet_applications) }
+  end
 
+  describe "instance methods" do
+    
+    let!(:app_1) { Application.create!(name: "Bob", street_address: "466 Birch Road", city: "Birmingham", state: "Alabama", zip_code: "35057", description: "description", status: "In Progress" ) }
+    
+    describe "#full_address" do
+      it "formats full addresses" do
+        expect(app_1.full_address).to eq("466 Birch Road Birmingham, Alabama, 35057")
+      end
+    end
   end
 end
